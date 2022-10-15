4018f608fe01fcfb427304b63761e95af156c73a
tsx358 <97471993+tsx358@users.noreply.github.com>
2022-10-14 21:16:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,36 +2,46 @@ const categories =  ["Human Organs","Geologic Period","Rocks", "Minerals","Disea
 
 const alphabet =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
+function reset(){
+const alphabetTemp = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-var j = Math.floor((alphabet.length)*Math.random());
+const category =  ["Human Organs","Geologic Period","Rocks", "Minerals","Disease Names","Natural disaster type","Fish species","Metals","Dinosaurs","Scientific instrument","Neurotransmitter","Insects","Constellations","Mammals","Genetic terms","Landforms","Ecosystem Types","Vaccine Types","River parts","Edible Seeds","Edible Nuts","Severe Weather Terms","Lake types","Fruit Trees","Plant Parts","Flightless Birds","Computer Parts","Forces and Motion","Human Bones","Element names","Plate Tectonic Terms","Reptiles","Weather vocab","Organelles","Engineering Vocab","Genetic Disorders","Trees","Scientific Laws","Muscle Names","Microscope Parts","Geologists","Acids","Physical Properties","Units of Measure","Physicists","Biologists","Chemists","Oceanography","Heredity","Simple Machines","Thermodynamics","Climatology","Birds","Fungi","Astronomers","Ocean Zones","Endangered Species","Wave Types","Immune System Parts","Eukaryote Parts","Poisons","Invasive Species","Weather Phenomena","Metalloids","Solar System Bodies"];
+  
+var i = Math.floor((category.length)*Math.random());
+document.getElementById("category1").innerHTML =  category[i];
+category.splice(i,1)
+var i = Math.floor((category.length)*Math.random());
+document.getElementById("category2").innerHTML =  category[i];
+category.splice(i,1)
+var i = Math.floor((category.length)*Math.random());
+document.getElementById("category3").innerHTML =  category[i];
+category.splice(i,1)
+var i = Math.floor((category.length)*Math.random());
+document.getElementById("category4").innerHTML =  category[i];
+category.splice(i,1)
+var i = Math.floor((category.length)*Math.random());
+document.getElementById("category5").innerHTML =  category[i];
 
-var i = Math.floor((categories.length)*Math.random());
-document.getElementById("category1").innerHTML =  categories[i];
+var j = Math.floor((alphabetTemp.length)*Math.random());
+document.getElementById("letter1").innerHTML =  alphabetTemp[j];
+alphabetTemp.splice(j,1)
+  //console.log(alphabetTemp)
+var j = Math.floor((alphabetTemp.length)*Math.random());
+document.getElementById("letter2").innerHTML =  alphabetTemp[j];
+alphabetTemp.splice(j,1)
+var j = Math.floor((alphabetTemp.length)*Math.random());
+document.getElementById("letter3").innerHTML =  alphabetTemp[j];
+alphabetTemp.splice(j,1)
+var j = Math.floor((alphabetTemp.length)*Math.random());
+document.getElementById("letter4").innerHTML =  alphabetTemp[j];
+alphabetTemp.splice(j,1)
+var j = Math.floor((alphabetTemp.length)*Math.random());
+document.getElementById("letter5").innerHTML =  alphabetTemp[j];
 
-var i = Math.floor((categories.length)*Math.random());
-document.getElementById("category2").innerHTML =  categories[i];
+for (let i=1; i<26; i++) {
+  console.log(i)
+  document.getElementById(i).value = ""
+}
 
-var i = Math.floor((categories.length)*Math.random());
-document.getElementById("category3").innerHTML =  categories[i];
-
-var i = Math.floor((categories.length)*Math.random());
-document.getElementById("category4").innerHTML =  categories[i];
-
-var i = Math.floor((categories.length)*Math.random());
-document.getElementById("category5").innerHTML =  categories[i];
-
-
-var j = Math.floor((alphabet.length)*Math.random());
-document.getElementById("letter1").innerHTML =  alphabet[j];
-
-var j = Math.floor((alphabet.length)*Math.random());
-document.getElementById("letter2").innerHTML =  alphabet[j];
-
-var j = Math.floor((alphabet.length)*Math.random());
-document.getElementById("letter3").innerHTML =  alphabet[j];
-
-var j = Math.floor((alphabet.length)*Math.random());
-document.getElementById("letter4").innerHTML =  alphabet[j];
-
-var j = Math.floor((alphabet.length)*Math.random());
-document.getElementById("letter5").innerHTML =  alphabet[j];
+}
+reset()
